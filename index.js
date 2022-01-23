da5f01b48cbd5699a36eff10616627a865a3e22d
Lautaro-Gonzalez <lgonzalez@aracargroup.com>
2022-01-23 13:41:19 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var todoList = []
+
+function togleAddToDo() {
+    const userInput = prompt('Inserte un Tarea', "Ej.: Pasear al perro")
+    console.log(userInput)
+    todoList.push(userInput)
+}
+
+function showItems() {
+    todoList.forEach(el => document.write(el))
+}
